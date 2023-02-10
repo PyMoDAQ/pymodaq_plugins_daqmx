c6e6ac6dd196e92ef07365ec992de9ac2363740f
Aurore Finco <aurore.finco@umontpellier.fr>
2023-02-10 16:40:58 +0100

    using the pymodaq edge counter

--- src/pymodaq_plugins_daqmx/daq_viewer_plugins/plugins_0D/daq_0Dviewer_DAQmx_PLcounter.py
+++ src/pymodaq_plugins_daqmx/daq_viewer_plugins/plugins_0D/daq_0Dviewer_DAQmx_PLcounter.py
@@ -18,21 +18,12 @@ class DAQ_0DViewer_DAQmx_PLcounter(DAQ_Viewer_base):
     params = comon_parameters+[
        {"title": "Counting channel:", "name": "counter_channel",
           "type": "list", "limits": DAQmx.get_NIDAQ_channels(source_type="Counter")},
-        {"title": "Source settings:", "name": "source_settings",
-           "type": "group", "visible": True, "children": [
-             {"title": "Enable?:", "name": "enable", "type": "bool",
-                "value": False, },
-             {"title": "Photon source:", "name": "photon_channel",
-                "type": "list", "limits": DAQmx.getTriggeringSources()},
-             {"title": "Edge type:", "name": "edge", "type": "list",
-                "limits": Edge.names(), "visible": False},
-             {"title": "Level:", "name": "level", "type": "float",
-                "value": 1., "visible": False}]
-         },
+        {"title": "Photon source:", "name": "photon_channel",
+         "type": "list", "limits": DAQmx.getTriggeringSources()},
         {"title": "Clock frequency (Hz):", "name": "clock_freq",
             "type": "float", "value": 100., "default": 100., "min": 1},
         {'title': 'Clock channel:', 'name': 'clock_channel', 'type': 'list',
-             'limits': DAQmx.get_NIDAQ_channels(source_type='Counter')},
+             'limits': DAQmx.get_NIDAQ_channels(source_type='Counter')}
         ]
 
     def ini_attributes(self):
@@ -108,40 +99,13 @@ class DAQ_0DViewer_DAQmx_PLcounter(DAQ_Viewer_base):
         #         update = False  # we are already live
         #     self.live = kwargs['live']
             
-        #if update:
+        # if update:
         self.update_tasks()
             
-        self.controller["clock"].stop() # to ensure that the clock is available
-        self.controller["clock"].task.CfgImplicitTiming(DAQmx_Val_FiniteSamps, 1000)
-
-        try:
-            timeout = 2
-            self.controller["clock"].start()
-            #self.controller["clock"].task.WaitUntilTaskDone(timeout*2)
-        except Exception as e:
-            print(e)
-            self.emit_status(ThreadCommand('Update_Status',
-                                               ['Cannot start the counter clock']))
-            return
-        
-        self.controller["counter"].task.CfgImplicitTiming(DAQmx_Val_ContSamps, 1000)
-        self.controller["counter"].task.SetReadRelativeTo(DAQmx_Val_CurrReadPos)
-        self.controller["counter"].task.SetReadOffset(0)
-        self.controller["counter"].task.SetReadOverWrite(DAQmx_Val_DoNotOverwriteUnreadSamps)
-        
-        try:
-            self.controller["counter"].start()
-        except Exception as e:
-            print(e)
-            self.emit_status(ThreadCommand('Update_Status',
-                                               ['Cannot start the PL counter']))
-                
-        print("ready to read")
         read_data = self.controller["counter"].readCounter(2, counting_time=self.counting_time,
                                                            read_function="")
         print(read_data)
-        data_pl = 1e-3*np.sum(read_data)/self.counting_time
-            
+        data_pl = 1e-3*read_data/self.counting_time
         self.data_grabed_signal.emit([DataFromPlugins(name='PL', data=[data_pl],
                                                       dim='Data0D', labels=['PL (kcts/s)'])])
 
@@ -153,42 +117,23 @@ class DAQ_0DViewer_DAQmx_PLcounter(DAQ_Viewer_base):
 
     def update_tasks(self):
         """Set up the counting tasks in the NI card."""
-        # Create channels
-        self.create_channels()
-        # configure tasks
-        self.configure_tasks()
-        # connect everything
-        self.connect_channels()
+        # Create channel
+        self.counter_channel = Counter(name=self.settings.child("counter_channel").value(),
+                                       source="Counter", edge=Edge.names()[0])
 
-    def create_channels(self):
-        """ Create the channels in the NI card to update the tasks."""
-        clock_freq = self.settings.child("clock_freq").value()
-        self.clock_channel = ClockCounter(clock_freq,
-                                          name=self.settings.child("clock_channel").value(),
-                                          source="Counter")
-        
-        self.counter_channel = SemiPeriodCounter(5e6,
-                                    name=self.settings.child("counter_channel").value(),
-                                    source="Counter")
-       
-    def configure_tasks(self):
-        """ Configure the tasks in the NI card, by calling the update functions of each controller."""
-        self.controller["clock"].update_task(channels=[self.clock_channel],
-                                             # do not configure clock yet, so Nsamples=1
-                                             clock_settings=ClockSettings(Nsamples=1),
-                                             trigger_settings=TriggerSettings())
         self.controller["counter"].update_task(channels=[self.counter_channel],
-                                               clock_settings=ClockSettings(Nsamples=1),
-                                               trigger_settings=TriggerSettings())
-        
-    def connect_channels(self):
-        """ Connect together the channels for synchronization."""
-        # connect the pulses from the clock to the counter
-        self.controller["counter"].task.SetCISemiPeriodTerm(self.counter_channel.name,
-                                                '/'+self.clock_channel.name + "InternalOutput")
-        # define the source of ticks for the counter 
-        self.controller["counter"].task.SetCICtrTimebaseSrc(self.counter_channel.name,
-                            self.settings.child("source_settings", "photon_channel").value())
+                                               clock_settings=ClockSettings(
+                                                   source=self.settings.child("clock_channel"),
+                                                   frequency=self.settings.child("clock_freq"),
+                                                   edge=Edge.names()[0],
+                                                   repetition=True,
+                                                   Nsamples=1),
+                                               trigger_settings=TriggerSettings(
+                                                   trig_source=self.settings.child("photon_channel"),
+                                                   enable=True,
+                                                   edge=Edge.names()[0],
+                                                   level=0.5)
+                                               )
         
         
 if __name__ == '__main__':
