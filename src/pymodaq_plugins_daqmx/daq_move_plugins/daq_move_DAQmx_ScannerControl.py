3ad95b849f51d0147871545d45c4c3614e2178d4
Goéland <aurore.finco@umontpellier.fr>
2023-08-22 15:17:38 +0200

    Scanner working, but clock issue when using both at the same time, need for another module

--- src/pymodaq_plugins_daqmx/daq_move_plugins/daq_move_DAQmx_ScannerControl.py
+++ src/pymodaq_plugins_daqmx/daq_move_plugins/daq_move_DAQmx_ScannerControl.py
@@ -1,5 +1,6 @@
 import numpy as np
-from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_parameters_fun, main  # common set of parameters for all actuators
+from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_parameters_fun, main  # common set of
+# parameters for all actuators
 from pymodaq.utils.daq_utils import ThreadCommand # object used to send info back to the main thread
 from pymodaq.utils.parameter import Parameter
 
@@ -11,7 +12,10 @@ from PyDAQmx import DAQmx_Val_FiniteSamps
 import PyDAQmx
 
 class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
-    """Plugin to control piezo scanners with a NI card.
+    """Plugin to control a piezo scanner with a NI card. This modules requires a clock channel to handle the
+    timing of the movement and display the position. Avoid using several scanners (ie several analog outputs)
+    with a single clock, this creates conflicts in the use of the clock channel. For this purpose, use the module
+    MultipleScannerControl.
 
     This object inherits all functionality to communicate with PyMoDAQ Module through inheritance via DAQ_Move_base
     It then implements the particular communication with the instrument
@@ -39,7 +43,7 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
     def ini_attributes(self):
         self.controller = None
         self.clock = None
-        self.step_size = 100.0 ## in nm! be careful with the scaling param
+        self.step_size = 100.0  # in nm! be careful with the scaling param
         self.step_time = 10e-3
         self.number_steps = 1
         self.conv_factor = 7500.0
@@ -69,7 +73,7 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
                 self.clock.task.GetCOCount(self.clock_channel.name, PyDAQmx.byref(current_step_index))
                 index = self.init_step_index - current_step_index.value
                 voltage = self.voltage_list[min(int(index/2), len(self.voltage_list)-1)]
-            except: # when the task did not start
+            except:  # when the task did not start
                 voltage = self.voltage_list[0]
         
         # if we do only one step, we do not care, there is no timing anyway.
@@ -82,6 +86,7 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
 
     def close(self):
         """ Terminate the communication protocol"""
+        print("move_done received, closing task")
         self.clock.close()
         self.controller.close()
 
@@ -128,15 +133,15 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
         self.step_time = self.settings.child("step_time").value()*1e-3
         self.conv_factor = self.settings.child("conv_factor").value()
 
-        # we need to close the clock once the move is done, the free
-        # the counter ressource
+        # we need to close the clock once the move is done, to free
+        # the counter resource
         self.move_done_signal.connect(self.close)
         
         try:
             self.update_task()
             initialized = True
             info = "NI card based piezo scanner control."
-            self.move_abs(0.0) # to avoid bad initial positioning because
+            self.move_abs(0.0)  # to avoid bad initial positioning because
             # we can't read the actual value from the NI card.
         except Exception as e:
             print(e)
@@ -155,7 +160,7 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
         if value == 0.0:
             value = 1.0
         self.close()
-        value = self.check_bound(value)  #if user checked bounds, the defined bounds are applied here
+        value = self.check_bound(value)  # if user checked bounds, the defined bounds are applied here
         self.target_value = value
         value = self.set_position_with_scaling(value)  # apply scaling if the user specified one
         self.move_scanner()
@@ -169,8 +174,8 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
         value: (float) value of the relative target positioning
         """
         self.close()
-        value = self.check_bound(self.current_position + value) - self.current_position
-        self.target_value = value + self.current_position
+        value = self.check_bound(self.current_value + value) - self.current_value
+        self.target_value = value + self.current_value
         if self.target_value == 0.0:
             self.target_value = 1.0
         value = self.set_position_relative_with_scaling(value)
@@ -226,16 +231,21 @@ class DAQ_Move_DAQmx_ScannerControl(DAQ_Move_base):
     def prepare_voltage_list(self):
         """Generates the list of voltages to move smoothly the scanner."""
         # if the desired step size is larger than the movement to realize, only one value
-        if np.abs(self.target_position - self.current_position) <= self.step_size:
-            self.voltage_list = np.array([self.target_position])/self.conv_factor
+        if np.abs(self.target_value - self.current_value) <= self.step_size:
+            self.voltage_list = np.array([self.target_value])/self.conv_factor
         # otherwise we do steps
         else:
-            pos_list = np.arange(min(self.current_position, self.target_position),
-                                 max(self.current_position, self.target_position)+self.step_size,
+            pos_list = np.arange(min(self.current_value, self.target_value),
+                                 max(self.current_value, self.target_value)+self.step_size,
                                  self.step_size)
             # we need to start from the beginning
-            if pos_list[0] != self.current_position:
+            if pos_list[0] != self.current_value:
                 pos_list = pos_list[::-1]
+
+            # we ensure that the last value is the target, otherwise we might get caught in a loop
+            # if the position goes to target from more than epsilon.
+            pos_list[-1] = self.target_value
+            
             # convert to voltage
             self.voltage_list = pos_list/self.conv_factor
         self.number_steps = len(self.voltage_list)
