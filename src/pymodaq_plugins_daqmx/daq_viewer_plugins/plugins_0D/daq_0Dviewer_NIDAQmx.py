8288ab9abec99fb596291f2fb23d87bb6f6d5270
Sebastien <sebastien.guerrero@insa-lyon.fr>
2024-12-05 17:36:36 +0100

    Attributes & methods from inheritance

--- src/pymodaq_plugins_daqmx/daq_viewer_plugins/plugins_0D/daq_0Dviewer_NIDAQmx.py
+++ src/pymodaq_plugins_daqmx/daq_viewer_plugins/plugins_0D/daq_0Dviewer_NIDAQmx.py
@@ -12,204 +12,41 @@ class DAQ_0DViewer_NIDAQmx(DAQ_NIDAQmx_Viewer):
     """
     Plugin for a 0D data visualization & acquisition with various NI modules plugged in a NI cDAQ.
     """
+
     channels_ai: str
     clock_settings_ai: str
     dict_device_input: dict
     live: bool
+    data_tot: list
+    Naverage: int
+    ind_average: int
 
-    params = comon_parameters+[
+    params = viewer_params + [
         {'title': 'Display type:', 'name': 'display', 'type': 'list', 'limits': ['0D', '1D']},
         {'title': 'Module ref. :', 'name': 'module', 'type': 'list', 'limits': DAQmx.get_NIDAQ_devices()[1],
          'value': DAQmx.get_NIDAQ_devices()[1][0]
          },
-        {'title': 'Analog Source :', 'name': 'ai_srce', 'type': 'list',
-         'limits': DAQ_NIDAQ_source.names(),
-         'value': DAQ_NIDAQ_source.names()[0]
-         },
-        {'title': 'Analog Type :', 'name': 'ai_type', 'type': 'list',
-         'limits': DAQ_analog_types.names(),
-         'value': DAQ_analog_types.names()[0]
-         },
-        {'title': 'Min. value:', 'name': 'ai_min', 'type': 'float', 'value': -80e-3, 'min': -1e4},
-        {'title': 'Max. value:', 'name': 'ai_max', 'type': 'float', 'value': 80e-3, 'max': 1e4},
-        {'title': 'Clock settings:', 'name': 'clock_settings', 'type': 'group', 'children': [
-            {'title': 'Frequency Acq.:', 'name': 'frequency', 'type': 'int', 'value': 3, 'min': 1},
-            {'title': 'Nsamples:', 'name': 'Nsamples', 'type': 'int', 'value': 10, 'default': 100, 'min': 1},
-            {'title': 'Acquisition mode:', 'name': 'acqmode', 'type': 'list',
-             'limits': ['Continuous', 'Finite'], 'value': 'Continous'},
-            {"title": "Counting channel:", "name": "counter_channel", "type": "list",
-             "limits": DAQmx.get_NIDAQ_channels(source_type="Counter")},
-        ]},
-        ]
+        ] + DAQ_NIDAQmx_base.params
 
     def __init__(self, parent=None, params_state=None):
         super().__init__(parent, params_state)
 
     def ini_attributes(self):
+        super().ini_attributes()
         self.controller: DAQmx = None
         self.channels_ai = None
         self.clock_settings_ai = None
         self.dict_device_input = None
         self.live = False  # True during a continuous grab
-
-    def commit_settings(self, param: Parameter):
-        """Apply the consequences of a change of value in the detector settings
-
-        Parameters
-        ----------
-        param: Parameter
-            A given parameter (within detector_settings) whose value has been changed by the user
-        """
-        # if param.name() == "frequency":
-        self.update_tasks()
-
-    def ini_detector(self, controller=None):
-        """Detector communication initialization
-
-        Parameters
-        ----------
-        controller: (object)
-            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
-            (Master case)
-
-        Returns
-        -------
-        info: str
-        initialized: bool
-            False if initialization failed otherwise True
-        """
-        logger.info("Detector 0D initialized")
-
-        try:
-            self.controller = self.ini_detector_init(controller, DAQmx())
-            self.update_tasks()
-            initialized = True
-            info = "DAQ_0D initialized"
-            self.controller.update_NIDAQ_devices()
-            self.controller.update_NIDAQ_channels()
-            logger.info("Detected devices: {}".format(self.controller.devices))
-            self.settings.child('devices').setValue(str(self.controller.devices))
-            logger.info("Detected channels: {}".format(self.controller.channels))
-            self.settings.child('channels').setValue(str(self.controller.channels))
-        except Exception as err:
-            logger.error(err)
-            initialized = False
-            info = "Error"
-        return info, initialized
+        self.data_tot = None
+        self.Naverage = 1
+        self.ind_average = 0
 
     def close(self):
-        """Terminate the communication protocol"""
+        self.live = False
+        self.controller.stop()
         self.controller.close()
         pass
-
-    def grab_data(self, Naverage=1, **kwargs):
-        """Start a grab from the detector
-
-        Parameters
-        ----------
-        Naverage: int
-            Number of hardware averaging not relevant here.
-        kwargs: dict
-            others optionals arguments
-        """
-        self.update_tasks()
-
-        data_from_task = self.controller._task.read()
-        logger.info("DATA = {}".format(data_from_task))
-
-        self.emit_data(data_from_task)
-
-    def emit_data(self, data_measurement):
-        logger.info("OOOOOOOOOOOOO {}".format([chan.name for chan in self.channels_ai]))
-        # dte = DataToExport(name='NIDAQmx',
-        #                    data=[DataFromPlugins(name='NI Analog Input 01',
-        #                                          data=[np.array([data_measurement[0]])],
-        #                                          # dim=f'Data{self.settings.child("display").value()}',
-        #                                          dim=f'Data0D',
-        #                                          labels=[self.channels_ai[0].name],
-        #                                          ),
-        #                          ])
-        dte = DataToExport(name='NIDAQmx',
-                           data=[DataFromPlugins(name='NI Analog Input 02',
-                                                 data=[np.array([data_measurement[2]])],
-                                                 # dim=f'Data{self.settings.child("display").value()}',
-                                                 dim=f'Data0D',
-                                                 labels=[self.channels_ai[2].name],
-                                                 ),
-                                 ])
-
-        # # Dictionary linking devices to channel's physical quantities
-        # self.dict_device_input = {self.controller.devices[1]: [self.channels_ai[0].name, self.channels_ai[1].name],
-        #                           self.controller.devices[3]: self.channels_ai[2].name}
-        # # self.controller.devices #ci_meas_types
-        # logger.info("DICT DEVICE LABEL {}".format(self.dict_device_input))
-        # logger.info(self.dict_device_input.get(self.controller.devices[1]))
-        # a = 0
-        # b = 0
-        # gen = [[ai for ai in self.dict_device_input.get(device)]
-        #        for device in self.dict_device_input if not isinstance(self.dict_device_input.get(device), str)] + \
-        #       [self.dict_device_input.get(device)
-        #        for device in self.dict_device_input if isinstance(self.dict_device_input.get(device), str)]
-        #
-        # logger.info("DATA TO EXPORT = {}".format(gen))
-        #
-        #
-        #
-        # dte = DataToExport(name='NIDAQmx',
-        #                    data=[DataFromPlugins(name=self.dict_device_input[device],
-        #                                          data=[np.array([data_measurement[i]])
-        #                                                for i in range(len(data_measurement))],
-        #                                          dim='Data0D',
-        #                                          labels=[ai for ai in self.dict_device_input.get(device)]
-        #                                          ) for device in self.dict_device_input])
-        self.dte_signal.emit(dte)
-
-    def stop(self):
-        """Stop the current grab hardware wise if necessary"""
-        self.close()
-        self.emit_status(ThreadCommand('Update_Status', ['Acquisition stopped.']))
-        return ''
-
-    def update_tasks(self):
-        """Set up the tasks in the NI card."""
-        # Create channels
-        #####################
-        '''
-        Idée:
-        for Device in Devices:
-            self.channels_from_device(Device01) = [AIchannel(),AIchannel()......]
-            self.channels_from_device(Device02) = [AIthermochannel(),AIthermochannel()......]
-        Emit by mode (as for keithley) === by device
-        
-        '''
-        self.channels_ai = [AIThermoChannel(name="cDAQ1Mod1/ai0",
-                                            source='Analog_Input',
-                                            analog_type='Thermocouple',
-                                            value_min=-20.,
-                                            value_max=1300.,
-                                            thermo_type=DAQ_thermocouples.K,
-                                            ),
-                            AIThermoChannel(name="cDAQ1Mod1/ai1",
-                                            source='Analog_Input',
-                                            analog_type='Thermocouple',
-                                            value_min=-20.,
-                                            value_max=1300.,
-                                            thermo_type=DAQ_thermocouples.K,
-                                            ),
-                            AIChannel(name="cDAQ1Mod3/ai0",
-                                      source='Analog_Input',
-                                      analog_type='Voltage',
-                                      value_min=-10.,
-                                      value_max=10.,
-                                      termination=DAQ_termination.Diff,
-                                      ),
-                            ]
-        self.clock_settings_ai = ClockSettings(frequency=self.settings.child('clock_settings', 'frequency').value(),
-                                               Nsamples=self.settings.child('clock_settings', 'Nsamples').value(),
-                                               edge=Edge.Rising,
-                                               repetition=self.live)
-
-        self.controller.update_task(self.channels_ai, self.clock_settings_ai)
 
 
 if __name__ == '__main__':
